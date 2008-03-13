06be56b1c74c77db09ad1f44824b51606cd5ab0d
ludovic <ludovic@devhost.(none)>
2008-03-14 00:17:26 +0800

    added few comments on the example; added a TODO text file

--- example/ex_simple_inference.ml
+++ example/ex_simple_inference.ml
@@ -1,8 +1,8 @@
 open Fuzlog
 open Inference
 
-let doit () = 
-    let fuzIO = FuzIO.create () in
+let create_model () =
+    (* define few liguistic variables *)
     let big = VarLing.create
         "BIG"
         (FuzzySet.create_triangle 5.0 15.0)
@@ -11,12 +11,14 @@ let doit () =
         "FAST"
         (FuzzySet.create_triangle 90.0 110.0)
     in
-    let ctrl1 = Controller.create
-        Norm.minimum
-        Implication.larsen
-        IsAlso.maximum
-        Defuzzyfication.barycenter
+    (* a model is a set of functions and a set of rules *)
+    Controller.create
+        Norm.minimum (* how to compute "a AND b" in rule premisse *)
+        Implication.larsen (* definition of implication operator *)
+        IsAlso.maximum (* define "output IS V1 AND output IS V2" in conclusion *)
+        Defuzzyfication.barycenter (* from fuzzy value to crisp one *)
         [
+            (* Rule1: IF in1 IS big AND in2 IS BIG THEN ou1 IS fast *)
             (
                 (Premisse.connect_and
                     (Premisse.create_input "input1" big)
@@ -24,21 +26,33 @@ let doit () =
                 ),
                 (Conclusion.create_output "output1" fast)
             );
+            (* Rule2: IF in2 IS BIG THEN ou2 IS fast *)
             (
                 (Premisse.create_input "input2" big),
                 (Conclusion.create_output "output2" fast)
             );
+            (* Rule2: IF in2 IS BIG THEN ou1 IS fast *)
             (
                 (Premisse.create_input "input2" big),
                 (Conclusion.create_output "output1" fast)
             );
         ]
-    in
+
+let doit () = 
+    (* model is the fuzzy controller *)
+    let ctrl1 = create_model () in
+    (* all input/output to model are set/get through FuzIO:
+    this is to enable to use the same model for multiple control state
+    *)
+    let fuzIO = FuzIO.create () in
+    (* set some inputs *)
     FuzIO.clear fuzIO;
     FuzIO.set_input fuzIO "input1" 6.0;
     FuzIO.set_input fuzIO "input2" 7.0;
+    (* eval the fuzzy inference *)
     ignore(Controller.eval ctrl1 fuzIO);
     FuzIO.debug fuzIO;
+    (* get some output after defuzzification *)
     List.iter
         (fun (name, value) ->
             Printf.printf "Defuzz: %s=%f\n" name value
